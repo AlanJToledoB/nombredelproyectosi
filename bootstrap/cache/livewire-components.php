07c0093fc45858fbb12fa1cefb4360fc09aa7958
Alan Joel Toledo Barrientos <107812467+AlanJToledoB@users.noreply.github.com>
2023-07-14 17:52:44 -0300

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,4 @@
+<?php return array (
+  'admin.users-index' => 'App\\Http\\Livewire\\Admin\\UsersIndex',
+  'admin.users-index.blade.php' => 'App\\Http\\Livewire\\Admin\\UsersIndex\\Blade\\Php',
+);
